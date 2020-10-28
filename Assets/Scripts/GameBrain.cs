098ffdf2e9162ece3e1a94c487e530dcedc6d4ae
Will <willthepeacock@gmail.com>
2020-10-27 18:45:50 -0700

    Post lesson commit

---
+++ Assets/Scripts/GameBrain.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameBrain : MonoBehaviour {
+	public PlayerController playerController;
+
+    void Start() {
+        
+    }
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+			OnGameComplete();
+		}
+    }
+
+	public void OnGameComplete() {
+		StartCoroutine(GameCompleteCo());
+	}
+
+	private IEnumerator GameCompleteCo() {
+		playerController.enabled = false;
+		Rigidbody2D playerRB = playerController.GetComponent<Rigidbody2D>();
+		playerRB.velocity = Vector2.zero;
+		playerRB.angularVelocity = 0.0f;
+		playerRB.bodyType = RigidbodyType2D.Static;
+
+		yield return new WaitForSeconds(1.0f);
+
+		AsyncOperation ayncLoad = SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().name);
+
+		while (!ayncLoad.isDone) {
+			yield return null;
+		}
+
+	}
+}
---
+++ Assets/Scripts/OrbBehavior.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbBehavior : MonoBehaviour
+{
+	public GameBrain gameBrain;
+
+	private bool hasCollided = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	private void OnTriggerEnter2D(Collider2D collision) {
+		if (!hasCollided && collision.gameObject.layer == LayerMask.NameToLayer("Player")) {
+			gameBrain.OnGameComplete();
+
+			hasCollided = true;
+		}
+	}
+}
